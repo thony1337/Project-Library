ce3c0c43fc96cda137eebac82f6c5fbd77b95b70
thony1337 <anthonyymartins@gmail.com>
2026-04-16 16:48:33 -0300

    add: 5 book examples;

--- main.js
+++ main.js
@@ -13,7 +13,12 @@ function addBookToLibrary(name, author, pages){
 }
 function displayBook() {
  myLibrary.forEach(item => {
-   console.log(item);
+   console.table(item);
  })
  }
-
+let book1 = addBookToLibrary('Mes confitures', 'Christine Ferber', '300');
+let book2 = addBookToLibrary('Call of the ktchulu', 'H.P Lovecraft', '190');
+let book3 = addBookToLibrary('Flordelis a pastora do diabo', 'Ulisses Campbell', '130');
+let book4 = addBookToLibrary('Elize Matsunaga', 'Ulisses Campbell', '150');
+let book5 = addBookToLibrary('Learning how to learn', 'Barbara Oakley', '200');
+displayBook();
